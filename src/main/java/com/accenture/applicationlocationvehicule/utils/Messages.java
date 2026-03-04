cec1b4e0a76396f539dde411410969484b8a338c
oceane.edde <oceane.edde@gmx.fr>
2026-03-04 15:22:22 +0100

    messages

---
+++ src/main/java/com/accenture/applicationlocationvehicule/utils/Messages.java
@@ -0,0 +1,125 @@
+package com.accenture.applicationlocationvehicule.utils;
+
+import lombok.Data;
+
+public class Messages {
+
+
+    //---CAR MESSAGES---
+
+    public static final String MESSAGES_ERROR_CAR_NOTFOUND = "car.id.notfound";
+    public static final String MESSAGES_ERROR_CAR_BRAND = "car.brand.null";
+    public static final String MESSAGES_ERROR_CAR_MODEL = "car.model.null";
+    public static final String MESSAGES_ERROR_CAR_COLOR = "car.color.null";
+    public static final String MESSAGES_ERROR_CAR_FUELTYPE = "car.fueltype.null";
+    public static final String MESSAGES_ERROR_CAR_DAILYRATE_NULL = "car.dailyrate.null";
+    public static final String MESSAGES_ERROR_CAR_DAILYRATE_MIN = "car.dailyrate.min";
+    public static final String MESSAGES_ERROR_CAR_MILEAGE_NULL = "car.mileage.null";
+    public static final String MESSAGES_ERROR_CAR_MILEAGE_MIN = "car.mileage.min";
+    public static final String MESSAGES_ERROR_CAR_ACTIVE = "car.active.null";
+    public static final String MESSAGES_ERROR_CAR_PARKREMOVE = "car.parkremove.null";
+    public static final String MESSAGES_ERROR_CAR_LICENSES = "car.lisences.null";
+    public static final String MESSAGES_ERROR_CAR_NBPLACES = "car.nbplaces.invalid";
+    public static final String MESSAGES_ERROR_CAR_NBDOORS = "car.nbdoors.invalid";
+    public static final String MESSAGES_ERROR_CAR_CONDITIONINGAIR = "car.conditioningair.null";
+    public static final String MESSAGES_ERROR_CAR_TRANSMISSION = "car.transmission.null";
+    public static final String MESSAGES_ERROR_CAR_TYPES = "car.types.null";
+
+
+    //---MOTORCYCLE MESSAGES---
+
+    public static final String MESSAGES_ERROR_MOTORCYCLE_NOTFOUND = "motorcycle.id.notfound";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_BRAND = "motorcycle.brand.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_MODEL = "motorcycle.model.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_COLOR = "motorcycle.color.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_FUELTYPE = "motorcycle.fueltype.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_DAILYRATE_NULL = "motorcycle.dailyrate.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_DAILYRATE_MIN = "motorcycle.dailyrate.min";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_MILEAGE_NULL = "motorcycle.mileage.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_MILEAGE_MIN = "motorcycle.mileage.min";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_ACTIVE = "motorcycle.active.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_PARKREMOVE = "motorcycle.parkremove.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_LICENSES = "motorcycle.lisences.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_NBCYLINDREE = "motorcycle.nbcylindree.invalid";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_CYLINDREE = "motorcycle.cylindree.invalid";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_WEIGHT = "motorcycle.weight.invalid";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_POWERKW = "motorcycle.powerkw.invalid";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_SADDLEHEIGHT = "motorcycle.saddleheight.invalid";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_TRANSMISSION = "motorcycle.transmission.null";
+    public static final String MESSAGES_ERROR_MOTORCYCLE_TYPES = "motorcycle.types.null";
+
+
+    // --- BIKE MESSAGES ---
+
+    public static final String MESSAGES_ERROR_BIKE_NOTFOUND = "bike.id.notfound";
+    public static final String MESSAGES_ERROR_BIKE_NULL= "bike.null";
+    public static final String MESSAGES_ERROR_BIKE_BRAND = "bike.brand.invalid";
+    public static final String MESSAGES_ERROR_BIKE_MODEL = "bike.model.invalid";
+    public static final String MESSAGES_ERROR_BIKE_COLOR = "bike.color.invalid";
+    public static final String MESSAGES_ERROR_BIKE_DAILYRATE_NULL = "bike.dailyrate.null";
+    public static final String MESSAGES_ERROR_BIKE_DAILYRATE_MIN = "bike.dailyrate.min";
+    public static final String MESSAGES_ERROR_BIKE_MILEAGE_NULL = "bike.mileage.null";
+    public static final String MESSAGES_ERROR_BIKE_MILEAGE_MIN = "bike.mileage.min";
+    public static final String MESSAGES_ERROR_BIKE_ACTIVE = "bike.active.null";
+    public static final String MESSAGES_ERROR_BIKE_PARKREMOVE = "bike.parkremove.null";
+    public static final String MESSAGES_ERROR_BIKE_LICENSES = "bike.lisenses.null";
+    public static final String MESSAGES_ERROR_BIKE_WEIGHT = "bike.lisenses.null";
+    public static final String MESSAGES_ERROR_BIKE_FRAMESIZE = "bike.framesize.null";
+    public static final String MESSAGES_ERROR_BIKE_ELECTRIC = "bike.electric.null";
+    public static final String MESSAGES_ERROR_BIKE_CAPACITYBATTERY = "bike.capacitybattery.invalid";
+    public static final String MESSAGES_ERROR_BIKE_AUTONOMY = "bike.autonomy.invalid";
+    public static final String MESSAGES_ERROR_BIKE_DISCBRAKE = "bike.discbrake.null";
+    public static final String MESSAGES_ERROR_BIKE_TYPES = "bike.types.null";
+
+
+    // ---MOTORHOME MESSAGES---
+
+    public static final String MESSAGES_ERROR_MOTORHOME_NOTFOUND = "motorHome.id.notfound";
+    public static final String MESSAGES_ERROR_MOTORHOME_BRAND = "motorHome.brand.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_MODEL = "motorHome.model.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_COLOR = "motorHome.color.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_FUELTYPE = "motorHome.fueltype.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_DAYLIRATE_NULL = "motorHome.dailyrate.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_DAYLIRATE_MIN = "motorHome.dailyrate.min";
+    public static final String MESSAGES_ERROR_MOTORHOME_MILEAGE_NULL = "motorHome.mileage.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_MILEAGE_MIN = "motorHome.mileage.min";
+    public static final String MESSAGES_ERROR_MOTORHOME_ACTIVE = "motorHome.active.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_PARKREMOVE = "motorHome.active.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_LICENSES = "motorHome.lisences.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_NBPLACES = "motorHome.nbplaces.invalid";
+    public static final String MESSAGES_ERROR_MOTORHOME_PTAC = "motorHome.ptac.invalid";
+    public static final String MESSAGES_ERROR_MOTORHOME_HEIGHT = "motorHome.height.invalid";
+    public static final String MESSAGES_ERROR_MOTORHOME_KITCHEN = "motorHome.kitchen.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_NBBED = "motorHome.nbbed.invalid";
+    public static final String MESSAGES_ERROR_MOTORHOME_NBLINEN = "motorHome.bedlinen.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_FRIDGE = "motorHome.fridge.nulll";
+    public static final String MESSAGES_ERROR_MOTORHOME_SHOWER = "motorHome.shower.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_TYPES = "motorHome.types.null";
+    public static final String MESSAGES_ERROR_MOTORHOME_TRANSMISSION = "motorHome.transmission.null";
+
+
+
+    //TRUCK MESSAGES
+
+    public static final String MESSAGES_ERROR_TRUCK_NOTFOUND = "truck.id.notfound";
+    public static final String MESSAGES_ERROR_TRUCK_BRAND = "truck.brand.null";
+    public static final String MESSAGES_ERROR_TRUCK_MODEL = "truck.model.null";
+    public static final String MESSAGES_ERROR_TRUCK_COLOR = "truck.color.null";
+    public static final String MESSAGES_ERROR_TRUCK_FUELTYPE = "truck.fueltype.null";
+    public static final String MESSAGES_ERROR_TRUCK_DAILYRATE_NULL = "truck.dailyrate.null";
+    public static final String MESSAGES_ERROR_TRUCK_DAILYRAT_MIN = "truck.dailyrate.min";
+    public static final String MESSAGES_ERROR_TRUCK_MILEAGE_NULL = "truck.mileage.null";
+    public static final String MESSAGES_ERROR_TRUCK_MILEAGE_MIN = "truck.mileage.min";
+    public static final String MESSAGES_ERROR_TRUCK_ACTIVE = "truck.active.null";
+    public static final String MESSAGES_ERROR_TRUCK_PARKREMOVE = "truck.parkremove.null";
+    public static final String MESSAGES_ERROR_TRUCK_LICENSES = "truck.licenses.null";
+    public static final String MESSAGES_ERROR_TRUCK_NBPLACES = "truck.nbplaces.invalid";
+    public static final String MESSAGES_ERROR_TRUCK_LOADMAX = "truck.loadmax.invalid";
+    public static final String MESSAGES_ERROR_TRUCK_CAPACITY = "truck.capacity.invalid";
+    public static final String MESSAGES_ERROR_TRUCK_PTAC = "truck.ptac.invalid";
+    public static final String MESSAGES_ERROR_TRUCK_TRANSMISSION = "truck.transmission.null";
+    public static final String MESSAGES_ERROR_TRUCK_CONDITIONINGAIR = "truck.conditioningair.null";
+    public static final String MESSAGES_ERROR_TRUCK_TYPES = "truck.types.null";
+
+
+}
