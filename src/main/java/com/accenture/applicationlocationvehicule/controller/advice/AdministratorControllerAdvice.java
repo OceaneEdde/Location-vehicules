0b8982af34c5ed2a6dacc14634c202dd3c22287e
oceane.edde <oceane.edde@gmx.fr>
2026-03-06 16:55:01 +0100

    fix roles

---
+++ src/main/java/com/accenture/applicationlocationvehicule/controller/advice/AdministratorControllerAdvice.java
@@ -0,0 +1,87 @@
+package com.accenture.applicationlocationvehicule.controller.advice;
+
+import com.accenture.applicationlocationvehicule.exception.AdministratorException;
+import com.accenture.applicationlocationvehicule.exception.ClientException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AdministratorControllerAdvice {
+
+    private final MessageSource messageSource;
+
+    public AdministratorControllerAdvice(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(AdministratorException.class)
+    public ResponseEntity<ErrorDto> businessException(AdministratorException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ErrorDto(
+                        java.time.LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        e.getMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> entityNotFound(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ErrorDto(
+                        java.time.LocalDateTime.now(),
+                        HttpStatus.NOT_FOUND.value(),
+                        e.getMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> notReadable(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ErrorDto(
+                        java.time.LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Json invalide : " + e.getMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorsDto> validationErrors(MethodArgumentNotValidException ex) {
+
+        ErrorsDto errorsDto = new ErrorsDto(
+                java.time.LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getBindingResult().getAllErrors().stream()
+                        .map(error -> new ErrorValidDto(
+                                ((FieldError) error).getField(),
+                                messageSource.getMessage(error.getDefaultMessage(), null, LocaleContextHolder.getLocale())
+                        ))
+                        .toList()
+        );
+
+        return ResponseEntity.badRequest().body(errorsDto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> generic(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ErrorDto(
+                        java.time.LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        e.getMessage()
+                )
+        );
+    }
+}
---
+++ src/main/java/com/accenture/applicationlocationvehicule/model/enums/Roles.java
@@ -0,0 +1,6 @@
+package com.accenture.applicationlocationvehicule.model.enums;
+
+public enum Roles {
+    ADMIN,
+    USER
+}
